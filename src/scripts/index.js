a6ccf87a14d12ee0dc292f002a53abc98536db98
erzhang.chen <erzhang.chen@qunar.com>
2017-05-12 16:16:27 +0800

    响应式

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,26 @@
 
 require('libs/zepto.min.js');
 
+var screenWidth = document.body.clientWidth,
+    screenHeight = document.body.clientHeight,
+    originWidth = 375,
+    originHeight = 667;
 
+var Page = function() {
+    this.$body = $('.pageWrapper');
+    this.init();
+};
+Page.prototype = {
+    init: function() {
+        this._reset();
+    },
+    _reset: function() {
+        console.log(screenWidth, originWidth, screenWidth / originWidth, screenHeight / originHeight);
+        this.$body.css({
+            '-webkit-transform': 'scaleX(' + screenWidth / originWidth + ') scaleY(' + screenHeight / originHeight + ')',
+            transform: 'scaleX(' + screenWidth / originWidth + ') scaleY(' + screenHeight / originHeight + ')'
+        });
+    }
+};
 
-
+new Page();
